128a17b9bc928fbfa818ae10ee9361001eea0b7d
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-23 14:49:44 +0800

    added food  components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,20 @@
 <script></script>
 
 <template>
-
-<h1>Hello WEBPROG SF231 World!</h1>
-
-</template>
-
-<style></style>
+    <h1>Food</h1>
+    <food-item/>
+    <food-item/>
+    <food-item/>
+  </template>
+  
+  <script></script>
+  
+  <style>
+    #app > div {
+          border: dashed black 1px;
+          display: inline-block;
+          margin: 10px;
+          padding: 10px;
+          background-color: lightgreen;
+      }
+  </style>     
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('food-item', FoodItem)
+app.mount('#app')
