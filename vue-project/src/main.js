75aca5f5fbbedb38e51c22d088744fefcff79660
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-27 14:20:20 +0800

    upd

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -7,4 +7,5 @@ import PersonalProfile from './components/PersonalProfile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
+app.component('personal-profile', PersonalProfile)
 app.mount('#app')
