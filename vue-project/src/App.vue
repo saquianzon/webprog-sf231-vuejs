940098fd1f5963285a50abcd13e8974706ef6c25
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-27 14:14:36 +0800

    uhh

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
     <h1>Food</h1>
+    <personal-profile/> <br>
     <food-item/>
     <food-item2/>
     <food-item/>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
