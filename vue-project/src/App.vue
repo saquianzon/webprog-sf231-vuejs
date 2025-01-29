803ba4e8fee5ac1905543b83cf060159decd89b9
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-29 16:49:56 +0800

    eh??

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -11,6 +11,15 @@
     <food-item2/>
 </template>
   
-  <script></script>
+  <script>
+  <script type="module" src="/src/main.js"></script>
+  <script src="https://unpkg.com/vue@3" defer></script>
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+  <script src="js/main2.js" defer></script>
+  <script src="js/goals.js" defer></script>
+  <script src="js/survey.js" defer></script>
+  <script src="js/modal.js" defer></script>
+  </script>
   
   <style></style>     
