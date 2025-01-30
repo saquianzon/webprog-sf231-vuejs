e7873229c0022bab97148cef4c1a936a8f6fb918
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-01-30 13:04:09 +0000

    installed client lib

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+export const supabase = createClient('https://urryjibupecxjfloewsj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycnlqaWJ1cGVjeGpmbG9ld3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyMTc3NTgsImV4cCI6MjA1Mzc5Mzc1OH0.pNQu4qLmd7t9Cq4I0cwt1jwcMS_7V1AzUO-AtG8yRuI')
