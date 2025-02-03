7b8d441adf7dfc79af7171f482b686bc7e7570e8
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-02-03 14:57:16 +0800

    edit

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import PersonalProfile from './components/Country.vue'
+import PersonalProfile from './components/CommentForm.vue'
+import PersonalProfile from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('country', Country)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
