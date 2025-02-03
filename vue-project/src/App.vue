277ebeb44efea7c32bf2bebf42fab4f140404ec0
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-02-03 14:52:50 +0800

    added comment vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,32 +1,20 @@
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
-
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
 
     <h1>Food</h1>
+    <country/>
     <personal-profile/> <br>
     <food-item/>
     <food-item2/>
     <food-item/>
     <food-item2/>
+    <comment-form/>
+    <comment/>
+    
 </template>
   
   <script></script>
   
-  <style></style>     
+  <style></style>   
+  
+  
+  
