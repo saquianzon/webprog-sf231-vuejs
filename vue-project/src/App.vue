f076f0d385029c8e73c71f699ad990e1d6a26cbb
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-02-06 06:21:50 +0000

    oops

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,6 @@
 
 <template>
     <h1>Food</h1>
-    <country/>
     <personal-profile/> <br>
     <goals/> <br>
     <gallery/> <br>
@@ -10,8 +9,6 @@
     <food-item2/>
     <food-item/>
     <food-item2/>
-    <comment-form/>
-    <comment/>
 </template>
   
   <script>
